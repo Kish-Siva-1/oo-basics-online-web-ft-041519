8521e0ecb21d4aa3d29d5267bca9e7806da2649a
Kish Siva <rodes88@msn.com>
2019-04-22 21:10:09 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,13 @@
 class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
   
-  
+  def initialized
+    it 'gets initialized with a brand' do
+      expect{Shoe.new("Adidas")}.to_not raise_error
+  end
   
   def cobble
     shoe.condiiton = 'new'
   end 
-  
-  
   
 end
