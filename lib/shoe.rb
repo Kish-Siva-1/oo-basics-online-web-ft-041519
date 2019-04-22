5d53df9205bbedf53c336fc862b18fdd8148cdd3
Kish Siva <rodes88@msn.com>
2019-04-22 21:20:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,10 @@
 class Shoe
   
-  attr_accessor :brand, :color, :size, :material, :condition, :properties
+  attr_accessor :brand, :color, :size, :material, :condition
   attr_reader :cobble
   
   def initialized(brand_new = "Adidas")
     @brand = brand_new
-  end
-  
-  def properties(brand_new2)
-    @brand = brand_new2
   end
   
   def cobble
